1cec5aeba44f43e2aec11fef959fc8009b5f9caa
Tomiwa Akinrotim <tomiwa.akinrotimi@gmail.com>
2025-09-30 22:33:06 -0500

    Chapter 4 Programming Excercises

---
+++ Number2.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int num1, num2, num3;
+//
+//	cout << "Enter three numbers delimited by a space: ";
+//	cin >> num1 >> num2 >> num3;
+//	cout << endl;
+//
+//	if (num1 < num2 && num2 < num3) 
+//	{
+//		cout << "These are the numbers in ascending order: " << num1 << " " << num2 << " " << num3 << endl;
+//	}
+//	else if (num1 < num3 && num3 < num2)
+//	{
+//		cout << "These are the numbers in ascending order: " << num1 << " " << num3 << " " << num2 << endl;
+//	}
+//	else if (num2 < num1 && num1 < num3)
+//	{
+//		cout << "These are the numbers in ascending order: " << num2 << " " << num1 << " " << num3 << endl;
+//	}
+//	else if (num2 < num3 && num3 < num1)
+//	{
+//		cout << "These are the numbers in ascending order: " << num2 << " " << num3 << " " << num1 << endl;
+//	}
+//	else if (num3 < num1 && num1 < num2)
+//	{
+//		cout << "These are the numbers in ascending order: " << num3 << " " << num1 << " " << num2 << endl;
+//	}
+//	else if (num3 < num2 && num2 < num1)
+//	{
+//		cout << "These are the numbers in ascending order: " << num3 << " " << num2 << " " << num1 << endl;
+//	}
+//
+//	return 0;
+//}
