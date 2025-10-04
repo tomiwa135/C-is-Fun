bf58b4c470a883bb024d14d86edde69a588a8952
Tomiwa A. <96557665+tomiwa135@users.noreply.github.com>
2025-10-04 15:07:37 -0500

    Add files via upload

---
+++ Example5-5_telephoneDigitsSentinelWhileLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+const char SENTINEL = '#';
+
+int main() {
+	char letter;
+	int num;
+
+	num = static_cast<int>(letter) - static_cast<int>('A');
+
+
+	return 0;
+}
