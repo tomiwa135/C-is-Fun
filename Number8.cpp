9721ce7f96c412b7da6156e534216191e9de2ddf
Tomiwa A. <96557665+tomiwa135@users.noreply.github.com>
2025-10-04 15:11:40 -0500

    Update Number8.cpp

--- Number8.cpp
+++ Number8.cpp
@@ -1,43 +1,43 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//	int a, b;
-//	string operation;
-//
-//	cout << "Enter the two variables required \n";
-//	cin >> a >> b;
-//	cout << endl;
-//
-//	cout << "Please choose the operation to be performed (sum, subtraction, multiply, division) " << endl;
-//	cin >> operation;
-//	cout << endl;
-//
-//	if (operation == "sum") 
-//	{
-//		cout << "a + b = " << a + b << endl;
-//	}
-//	else if (operation == "subtraction")
-//	{
-//		cout << "a - b = " << a - b << endl;
-//	}
-//	else if (operation == "multiply")
-//	{
-//		cout << "a * b = " << a * b << endl;
-//	}
-//	else if (operation == "division")
-//	{
-//		if (b == 0)
-//		{
-//			cout << "Can't be divisible by 0" << endl;
-//		}
-//		else 
-//		{
-//			cout << "a / b = " << a / b << endl;
-//		}
-//	}
-//
-//	return 0;
-//
-//}
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	string operation;
+
+	cout << "Enter the two variables required \n";
+	cin >> a >> b;
+	cout << endl;
+
+	cout << "Please choose the operation to be performed (sum, subtraction, multiply, division) " << endl;
+	cin >> operation;
+	cout << endl;
+
+	if (operation == "sum") 
+	{
+		cout << "a + b = " << a + b << endl;
+	}
+	else if (operation == "subtraction")
+	{
+		cout << "a - b = " << a - b << endl;
+	}
+	else if (operation == "multiply")
+	{
+		cout << "a * b = " << a * b << endl;
+	}
+	else if (operation == "division")
+	{
+		if (b == 0)
+		{
+			cout << "Can't be divisible by 0" << endl;
+		}
+		else 
+		{
+			cout << "a / b = " << a / b << endl;
+		}
+	}
+
+	return 0;
+
+}
