6a13b06b9f1cf382310c8f1778fa4a70038937e4
Tomiwa A. <96557665+tomiwa135@users.noreply.github.com>
2025-10-04 15:10:21 -0500

    Update Number1.cpp

--- Number1.cpp
+++ Number1.cpp
@@ -1,26 +1,26 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//	int num;
-//
-//	cout << "Please enter a number: ";
-//	cin >> num;
-//	cout << endl;
-//
-//	if (num > 0) {
-//		cout << "This is a positive number " << num << endl;
-//	}
-//	else if (num == 0) {
-//		cout << "This number is zero " << num << endl;
-//	}
-//	else if (num < 0) {
-//		cout << "This is a negative number " << num << endl;
-//	}
-//	else {
-//		cout << "This is not a number" << endl;
-//	}
-//
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num;
+
+	cout << "Please enter a number: ";
+	cin >> num;
+	cout << endl;
+
+	if (num > 0) {
+		cout << "This is a positive number " << num << endl;
+	}
+	else if (num == 0) {
+		cout << "This number is zero " << num << endl;
+	}
+	else if (num < 0) {
+		cout << "This is a negative number " << num << endl;
+	}
+	else {
+		cout << "This is not a number" << endl;
+	}
+
+	return 0;
+}
